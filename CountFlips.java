971d2690eff9de42d9d23604e44e9ce94bf3c6fe
0krini <tpape10@icloud.com>
2026-03-24 19:20:06 -0400

    Implement coin flip counting in CountFlips.java

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,23 @@
 public class CountFlips {
   public static void main(String[] args) {
+     Coin coin = new Coin();
+
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    // Flip the coin 100 times
+    for (int i = 0; i < 100; i++) {
+      coin.flip();
+
+      if (coin.isHeads()) {
+        headsCount++;
+      } else {
+        tailsCount++;
+      }
+    }
+
+    // Print results
+    System.out.println("Number of Heads: " + headsCount);
+    System.out.println("Number of Tails: " + tailsCount);
   }
 }
